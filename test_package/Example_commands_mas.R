4ff5680f55d873042d682af8ffa8680b78548551
Martha Sudermann <masudermann@10-248-44-208.wireless.oregonstate.edu>
2022-12-08 15:00:23 -0800

    updates

--- test_package/Example_commands_mas.R
+++ test_package/Example_commands_mas.R
@@ -9,8 +9,8 @@ document()
 #Note-sample names should be sample1_xx_xx_R1.fastq.gz. The key is all sample names are unique before first underscore, and this sample name matches metadata.csv sample name.
 
 #You will need to modify directory names and file names
-directory_path<-"data" ##choose a directory for all downstream steps
-raw_path <-file.path(directory_path) #place your raw data files, csv files, and downloaded databases into raw_data subdirectory into your main directory
+directory_path<-"~/MonterreyBay" ##choose a directory for all downstream steps
+raw_path <-file.path(directory_path) #place your raw data files, csv files, and downloaded databases into your chosen directory
 primer_path <-file.path(raw_path, "primer_info.csv") ##modify .csv name or keep this name
 #Metadata file just needs sample_name one column, and primer_name in second column (this function is being tweaked-see example)
 metadata_path <-file.path(raw_path, "metadata.csv") ##modify .csv name or keep this name. The sample_name in the metadata sheet needs to match the first part (before first underscore), of the zipped raw FASTQ files
@@ -19,29 +19,41 @@ intermediate_path <- create_intermediate(directory_path)
 #create intermediate data folder in working directory
 #Now you are ready for actual commands
 
+
+#You will need to modify directory names and file names
+directory_path<-"~/rps10_metabarcoding_tool/test_package/data" ##choose a directory for all downstream steps
+raw_path <-file.path(directory_path) #place your raw data files, csv files, and downloaded databases into your chosen directory
+primer_path <-file.path(raw_path, "primer_info.csv") ##modify .csv name or keep this name
+#Metadata file just needs sample_name one column, and primer_name in second column (this function is being tweaked-see example)
+metadata_path <-file.path(raw_path, "metadata.csv") ##modify .csv name or keep this name. The sample_name in the metadata sheet needs to match the first part (before first underscore), of the zipped raw FASTQ files
+cutadapt_path<-"/Users/masudermann/miniconda3/bin/cutadapt"
+intermediate_path <- create_intermediate(directory_path)
+#create intermediate data folder in working directory
+#Now you are ready for actual commands
+ 
 #Further simplify
 #change names
 returnList<-prepare_reads(directory_path, primer_path, metadata_path, fastq_path, intermediate_path, maxN=0, multithread=TRUE)
-cut_trim(returnList,intermediate_path, cutadapt_path, verbose=TRUE, maxEE=5) 
+cut_trim(returnList,intermediate_path, cutadapt_path, verbose=TRUE, maxEE=5, truncQ=5, min_length=50) #ix cutadapt param!!!
 #add message to let user know which steps have already run if they are skipped, and also provide info on the params. Make it easier for user to re-run analysis. 
 
-asv_abund_matrix <- make_asv_abund_matrix(returnList, intermediate_path, returnList$cutadapt_data, verbose=TRUE,  maxMismatch = 0, multithread=TRUE) #check when 0 again
-#TODO adjust maxmismatch if there aren't alot of merged reads
+asv_abund_matrix <- make_asv_abund_matrix(returnList, intermediate_path, returnList$cutadapt_data, minOverlap=15, maxMismatch=2, verbose=TRUE, multithread=TRUE) #check when 0 again
+#TODO adjust maxmismatch if there aren't alot of merged reads                                                                                         
 
 #Command to prepare databases for downstream steps
 #TODO format like SILVA? 
-#create_ref_database(intermediate_path)
-#format_database_rps10(raw_path, "oomycetedb.fasta")
+create_ref_database(intermediate_path)
+format_database_rps10(raw_path, "oomycetedb.fasta")
 #If you included the ITS barcode in your analysis
 #Should also test pipeline just on ITS dataset (could also make your own fungal database)
-#format_database_unite(raw_path, "unite.fasta")
+format_database_unite(raw_path, "unite.fasta")
 #The remaining functions will be incorporated shortly
 
 #For just rps10 barcode-use only if you only dealing with one barcode
-#summary_table<-process_rps10_barcode(returnList, asv_abund_matrix, multithread = TRUE)
+summary_table<-process_rps10_barcode(returnList, asv_abund_matrix, multithread = TRUE)
 
 #For ITS and rps10 barcodes
-#summary_table2<-process_rps10_ITS_barcode(returnList, asv_abund_matrix, multithread = TRUE)
+summary_table2<-process_rps10_ITS_barcode(returnList, asv_abund_matrix, multithread = TRUE)
 #check outputs and examples with both databases
 #function to make phylosoq object
 #inputs
