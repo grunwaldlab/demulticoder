c64903626191ec6e93d252805f8208ca4e82d20c
Martha Sudermann <masudermann@10-249-251-235.wireless.oregonstate.edu>
2024-04-02 15:22:00 -0700

    Debug functions

--- Tests.R
+++ Tests.R
@@ -4,8 +4,8 @@ devtools::document()
 prepare_reads(
   maxN = 0, 
   data_directory = "~/demulticoder/inst/extdata", 
-  output_directory = "~/testing_package5", 
-  tempdir_id = "test11",
+  output_directory = "~/testing_package10", 
+  tempdir_id = "tempdir",
   tempdir_path="~/",
   overwrite_existing = FALSE)
 
@@ -19,11 +19,9 @@ make_asv_abund_matrix(
   overwrite_existing = TRUE)
 
 #still need to fix align file
-set.seed(2)
 assign_tax(
   analysis_setup,
   asv_abund_matrix,
-  tryRC = FALSE,
   db_its = "fungidb_larger3.fasta",
   db_rps10 = "oomycetedb.fasta",
   db_16s = "bacteriadb.fasta",
@@ -31,6 +29,19 @@ assign_tax(
   overwrite_existing=TRUE)
 
 convert_asv_matrix_to_objs(save_outputs=TRUE, overwrite=TRUE)
+
+load("~/demulticoder/inst/extdata/obj_dada_its.RData") 
+obj_dada_its<-obj_dada
+
+
+heat_tree(obj_dada_its,
+          node_label = taxon_names,
+          node_size = n_obs,
+          node_color = n_obs,
+          node_color_axis_label = "ASV count",
+          node_size_axis_label = "Total Abundance of Taxa",
+          layout = "da", initial_layout = "re")
+
 
 #TODO-cleanup functions again and revise documentation
 #fix align file
@@ -42,7 +53,7 @@ check()
 
 usethis::use_pkgdown()
 
-load("~/testing_package4/test11/asvabund_matrixDADA2_rps10.RData")
+load("~/testing_package8/test14/asvabund_matrixDADA2_rps10.RData")
 set.seed(2)
 taxa<-assignTaxonomy(
   asv_abund_matrix,
@@ -55,16 +66,21 @@ taxa<-assignTaxonomy(
   verbose = TRUE
 )
 
-set.seed(2)
-load("~/testing_package5/test11/")
+set.seed(1)
+load("~/testing_package10/tempdir/asvabund_matrixDADA2_sixteenS.RData")
 taxa<-assignTaxonomy(
   asv_abund_matrix,
-  "~/test11/sixteenS_reference_db.fa",
+  "~/bacteriadb.fasta",
   minBoot = 0,
-  tryRC = FALSE,
+  tryRC = TRUE,
   outputBootstraps = TRUE,
-  #taxLevels = c("Domain", "Kingdom", "Phylum", "Class", "Order", "Family", "Genus", "Species", "Reference"),
   multithread = TRUE,
   verbose = TRUE
 )
-write.table(taxa,file="~/test_rps10original2.out",sep="\t",quote=F)
+
+
+load("~/testing_package10/tempdir/TaxMatrix_rps10.RData")
+load("~/testing_package10/test14/.RData")
+
+write.table(taxa,file="~/soxteemS_norename.out",sep="\t",quote=F)
+write.table(tax_results,file="~/rps10_NAs_refdb.out",sep="\t",quote=F)
