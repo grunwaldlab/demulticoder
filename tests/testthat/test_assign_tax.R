41bc6d7a0d0ba16f703abe68a29b189d4bd9cf23
Martha Sudermann <sudermam@oregonstate.edu>
2025-04-30 14:57:27 -0700

    Quick fix to tests, if cutadapt is not installed

--- tests/testthat/test_assign_tax.R
+++ tests/testthat/test_assign_tax.R
@@ -1,11 +1,15 @@
-test_that("assign_taxonomy works correctly", {
-  data_directory <- system.file("extdata", package = "demulticoder")
-  analysis_setup <- prepare_reads(data_directory, overwrite_existing = TRUE)
-  cut_trim(analysis_setup, cutadapt_path = "/usr/bin/cutadapt", overwrite_existing = TRUE)
-  make_asv_abund_matrix(analysis_setup, overwrite_existing = TRUE)
-  assign_tax(analysis_setup, overwrite_existing = TRUE)
-  final_asv_matrix <- list.files(analysis_setup$directory_paths$output_directory, pattern = "^final_asv_abundance_matrix_*\\.RData$", full.names = TRUE)
-  asvplots <- list.files(analysis_setup$directory_paths$output_directory, pattern = "^track_reads_*\\.RData$", full.names = TRUE)
-  expect_true(all(file.exists(final_asv_matrix)))
-  expect_true(all(file.exists(asvplots)))
-})
+if (Sys.which("cutadapt") != "") {
+  test_that("assign_taxonomy works correctly", {
+    data_directory <- system.file("extdata", package = "demulticoder")
+    analysis_setup <- prepare_reads(data_directory, overwrite_existing = TRUE)
+    cut_trim(analysis_setup, cutadapt_path = "/usr/bin/cutadapt", overwrite_existing = TRUE)
+    make_asv_abund_matrix(analysis_setup, overwrite_existing = TRUE)
+    assign_tax(analysis_setup, overwrite_existing = TRUE)
+    final_asv_matrix <- list.files(analysis_setup$directory_paths$output_directory, pattern = "^final_asv_abundance_matrix_*\\.RData$", full.names = TRUE)
+    asvplots <- list.files(analysis_setup$directory_paths$output_directory, pattern = "^track_reads_*\\.RData$", full.names = TRUE)
+    expect_true(all(file.exists(final_asv_matrix)))
+    expect_true(all(file.exists(asvplots)))
+  })
+} else {
+  message("cutadapt is not installed, skipping tests.")
+}
--- tests/testthat/test_cut_trim.R
+++ tests/testthat/test_cut_trim.R
@@ -1,7 +1,13 @@
-test_that("cut_trim works correctly", {
-  data_directory <- system.file("extdata", package = "demulticoder")
-  analysis_setup <- prepare_reads(data_directory, overwrite_existing = TRUE)
-  cut_trim(analysis_setup, cutadapt_path = "/usr/bin/cutadapt", overwrite_existing = TRUE)
-  expect_true(all(file.exists(analysis_setup$data_tables$cutadapt_data$trimmed_path)))
-})
+if (Sys.which("cutadapt") != "") {
+  test_that("cut_trim works correctly", {
+    data_directory <- system.file("extdata", package = "demulticoder")
+    analysis_setup <- prepare_reads(data_directory, overwrite_existing = TRUE)
+    cut_trim(analysis_setup,
+             cutadapt_path = "/usr/bin/cutadapt", overwrite_existing = TRUE)
+    expect_true(all(file.exists(analysis_setup$data_tables$cutadapt_data$trimmed_path)))
+  })
+} else {
+  message("cutadapt is not installed, skipping tests.")
+}
+
 
